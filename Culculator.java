38624b6bf86b76f596446881373852a80103cac9
inouetetsuji <81937796+inouetetsuji@users.noreply.github.com>
2022-03-06 14:33:29 +0900

    Add files via upload

---
+++ Culculator.java
@@ -0,0 +1,24 @@
+
+package study;
+
+public class Culculator {
+
+	int a=10;
+	int b=20;
+	int c=30;
+	
+    protected int plus(int a) {
+        return a + 1;
+        System.out.println("変数が一つの場合は"+());
+    }
+
+    protected int plus(int a, int b) {
+        return a + b;
+        System.out.println ("変数が二つの場合は"+());
+    }
+
+    protected int plus(int a, int b, int c) {
+        return a + b + c;
+        System.out.println("変数が三つの場合は"+();
+    }
+}
