730f092a52c299dc76de35c35a84fc281676ea1b
inouetetsuji <motimawashimaeotoshi228@gmail.com>
2022-03-07 21:23:22 +0900

    Task2-3-1
    Task2-3-1

---
+++ Culculator.java
@@ -0,0 +1,19 @@
+
+package study;
+
+public class Culculator {
+
+	int a=10;
+	int b=20;
+	int c=30;
+	
+    protected int plus(int a) {
+        return a + 1;
+    }
+    protected int plus(int a, int b) {
+        return a + b;
+    }
+    protected int plus(int a, int b, int c) {
+        return a + b + c;
+    }
+}
