09b49fa05cbdd4ac6d5caeb5cac05057e069729f
inouetetsuji <81937796+inouetetsuji@users.noreply.github.com>
2022-03-17 13:15:25 +0900

    Add files via upload

--- Culculator.java
+++ Culculator.java
@@ -3,37 +3,14 @@ package study;
 
 public class Culculator {
 
-	int a=10;
-	int b=20;
-	int c=30;
 	
     protected int plus(int a) {
         return a + 1;
-<<<<<<< HEAD
-<<<<<<< HEAD
     }
     protected int plus(int a, int b) {
         return a + b;
     }
     protected int plus(int a, int b, int c) {
         return a + b + c;
-=======
-=======
->>>>>>> 38624b6bf86b76f596446881373852a80103cac9
-        System.out.println("変数が一つの場合は"+());
-    }
-
-    protected int plus(int a, int b) {
-        return a + b;
-        System.out.println ("変数が二つの場合は"+());
-    }
-
-    protected int plus(int a, int b, int c) {
-        return a + b + c;
-        System.out.println("変数が三つの場合は"+();
-<<<<<<< HEAD
->>>>>>> 38624b6bf86b76f596446881373852a80103cac9
-=======
->>>>>>> 38624b6bf86b76f596446881373852a80103cac9
     }
 }
--- Task.java
+++ Task.java
@@ -7,25 +7,11 @@ class Task extends Culculator{
      * タスクの実行
      */
     public void doTask() {
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 		// ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-          System.out.println("plusメソッドの引数が一つの場合"+super.plus(a+1));
-          System.out.println("plusメソッドの引数が二つの場合"+super.plus(a+b)); 	
-          System.out.println("plusメソッドの引数が三つの場合"+super.plus(a+b+c)); 	
+          System.out.println("plusメソッドの引数が一つの場合"+super.plus(10));
+          System.out.println("plusメソッドの引数が二つの場合"+super.plus(10,20)); 	
+          System.out.println("plusメソッドの引数が三つの場合"+super.plus(10,20,30)); 	
 	}
     
-=======
-=======
->>>>>>> 38624b6bf86b76f596446881373852a80103cac9
-        // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
-        // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-
-    }
-<<<<<<< HEAD
->>>>>>> 38624b6bf86b76f596446881373852a80103cac9
-=======
->>>>>>> 38624b6bf86b76f596446881373852a80103cac9
 }
